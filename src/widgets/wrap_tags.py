0f2d6cd8200149c17183ef119accfb2009b97c48
DaemonWhite <daemonwhites@vivaldi.net>
2025-10-01 12:08:28 +0200

    add: fix empty tag

--- src/widgets/wrap_tags.py
+++ src/widgets/wrap_tags.py
@@ -13,7 +13,7 @@ class WrapTags(Adw.WrapBox):
     def add_tag(self, tag_name: str):
         tag = Tag(label=tag_name)
         tag.close.connect("clicked", self._remove_tag, tag)
-        if tag_name not in self._know_tags:
+        if tag_name not in self._know_tags and len(tag_name) > 1:
             self.append(tag)
             self._know_tags.append(tag_name)
 
