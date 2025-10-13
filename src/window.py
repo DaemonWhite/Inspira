16006c0216be1c650f9922567a1055243a143554
DaemonWhite <daemonwhites@vivaldi.net>
2025-10-13 10:25:02 +0200

    fix: loaded_api deleted previously

--- src/window.py
+++ src/window.py
@@ -150,7 +150,6 @@ class InspiraWindow(Adw.ApplicationWindow):
 
     def loadImage(self, args):
         if self.image_drop_down.get_model().get_n_items() < 1:
-            self.loadedImage()
             print("Error not api enabled")
             return
         selected_api = self.image_drop_down.get_selected_item().get_string()
